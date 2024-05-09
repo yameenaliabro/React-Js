24abcaf4d9e89eaa93787dace967601d3190c84e
Muhammad Yameen <muhammadyameen5432@gmail.com>
2024-05-10 01:27:43 +0500

    feat: add the calendaly

---
+++ src/Calendaly/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function CalendlyComponent() {
+    return (
+        <div style={{ height: '1000px' }}>
+            <iframe
+                title="Calendly"
+                src="https://calendly.com/muhammadyameen5432"
+                width="100%"
+                height="100%"
+                frameBorder="0"
+            />
+        </div>
+    );
+}
+
+export default CalendlyComponent;
--- src/components/routes/index.tsx
+++ src/components/routes/index.tsx
@@ -4,14 +4,15 @@ import TodoApplication from '../todo'
 import Calculator from '../Calculator'
 import MultiStepsForm from '../multi-form'
 import SSoLogin from '../sso'
+import CalendlyComponent from '../../Calendaly'
 
 function MainRoutes() {
   return (
     <Routes>
-        <Route path='/' element={<TodoApplication/>} index/>
-        <Route path='/calculator' element={<Calculator/>}/>
-        <Route path='/multiform' element={<MultiStepsForm/>}/>
-        <Route path='/sso' element={<SSoLogin/>}/>
+      <Route path='/' element={<CalendlyComponent />} index />
+      <Route path='/calculator' element={<Calculator />} />
+      <Route path='/multiform' element={<MultiStepsForm />} />
+      <Route path='/sso' element={<SSoLogin />} />
     </Routes>
   )
 }
